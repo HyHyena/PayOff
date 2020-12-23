643d6efc444adf37b847a794ffba450285b07ec3
shovut <tbikba4ok@mail.ru>
2020-12-23 19:50:25 +0300

    it actually works

--- back/src/main/java/ru/dreamteam/utils/SignatureGenerator.java
+++ back/src/main/java/ru/dreamteam/utils/SignatureGenerator.java
@@ -3,16 +3,19 @@ package ru.dreamteam.utils;
 import lombok.SneakyThrows;
 import org.apache.tomcat.util.codec.binary.Base64;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
 
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
 
+@Component
 public class SignatureGenerator {
 
-    static String algorithm = "HmacSHA1";
+    @Value("${algorithm}")
+    String algorithm;
 
     @SneakyThrows
-    public static String generateSignature(String secret, String body, Long salt){
+    public String generateSignature(String secret, String body, Long salt){
         SecretKeySpec signingKey = new SecretKeySpec(secret.getBytes(), algorithm);
         Mac mac = Mac.getInstance(algorithm);
         mac.init(signingKey);
--- back/src/test/java/ru/dreamteam/utils/SignatureGeneratorTest.java
+++ back/src/test/java/ru/dreamteam/utils/SignatureGeneratorTest.java
@@ -2,17 +2,23 @@ package ru.dreamteam.utils;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class SignatureGeneratorTest {
+
+    @Autowired
+    SignatureGenerator signatureGenerator;
 
     @Test
     void generateSignature() {
         String body = "{\"amount\":20,\"currency\":\"RUB\",\"returnUrl\":\"https://example.com/\",\"method\":\"card\",\"orderId\":123456}";
         String secret = "D4219ACA4320F34497112FDA74A62EB2";
         Long salt = 1923840923842343L;
-        String signature = SignatureGenerator.generateSignature(secret, body, salt);
+        String signature = signatureGenerator.generateSignature(secret, body, salt);
         Assertions.assertEquals("8Pf+h8uUf664un/Bhz9jmRoxLkw=", signature);
     }
 }
